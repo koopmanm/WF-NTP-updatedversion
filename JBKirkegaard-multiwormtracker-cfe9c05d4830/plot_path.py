3966ac3f7aff2ef661277381ddda5d0d0f73648c
Quentin <qaep2@cam.ac.uk>
2019-11-15 14:31:36 +0000

    2to3 and pep8

--- JBKirkegaard-multiwormtracker-cfe9c05d4830/plot_path.py
+++ JBKirkegaard-multiwormtracker-cfe9c05d4830/plot_path.py
@@ -1,51 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import cPickle
-import tkFileDialog
+import pickle
+import tkinter.filedialog
+
 
 class Video:
-    def __init__(self,fname,grey=False):
+    def __init__(self, fname, grey=False):
         self.cap = cv2.VideoCapture(fname)
         self.fname = fname
-        self.name = "".join(fname.split(".")[:-1]).replace('/','_')
+        self.name = "".join(fname.split(".")[:-1]).replace('/', '_')
         self.len = self.cap.get(cv.CV_CAP_PROP_FRAME_COUNT) - start_frame
-        if limit_images_to and limit_images_to<(self.len-start_frame):
+        if limit_images_to and limit_images_to < (self.len - start_frame):
             self.len = limit_images_to
         self.grey = grey
         if grey:
-            for _ in xrange(100):
+            for _ in range(100):
                 ret, frame = self.cap.read()
                 if ret:
                     break
-            if len(frame.shape)==2:
+            if len(frame.shape) == 2:
                 self.grey = False
-            self.cap.set(cv.CV_CAP_PROP_POS_FRAMES,0)
-    def next(self):
+            self.cap.set(cv.CV_CAP_PROP_POS_FRAMES, 0)
+
+    def __next__(self):
         ret = False
-        for _ in xrange(100):
+        for _ in range(100):
             ret, frame = self.cap.read()
             if ret:
                 break
-            time.sleep(0.1*random.random())
+            time.sleep(0.1 * random.random())
         if ret:
             if self.grey:
-                return frame[:,:,0]
+                return frame[:, :, 0]
             else:
                 return frame
         else:
             raise StopIteration
-    def set_index(self,i):
-        self.cap.set(cv.CV_CAP_PROP_POS_FRAMES,i)
+
+    def set_index(self, i):
+        self.cap.set(cv.CV_CAP_PROP_POS_FRAMES, i)
+
     def restart(self):
         self.set_index(start_frame)
-    def __getitem__(self,i):
-        if i<0:
+
+    def __getitem__(self, i):
+        if i < 0:
             i += self.len
-        self.set_index(start_frame+i)
-        return self.next()
+        self.set_index(start_frame + i)
+        return next(self)
+
     def __len__(self):
         return int(self.len)
+
     def release(self):
         self.cap.release()
 
@@ -54,15 +61,15 @@ def plot_path(filename):
     saved_name = "/".join(filename.split('/')[:-1])
 
     plt.close()
-    plt.figure(figsize=(10,8))
+    plt.figure(figsize=(10, 8))
     colormap = cm.Set2
 
     with open(filename) as f:
-        track = cPickle.load(f)
+        track = pickle.load(f)
 
     particles = set(track['particle'])
-    colours = [colormap(i/float(len(particles)))
-                    for i in xrange(len(particles))]
+    colours = [colormap(i / float(len(particles)))
+               for i in range(len(particles))]
     rand = np.random.permutation(len(particles))
     for i, p in enumerate(particles):
         idx = track['particle'] == p
@@ -70,21 +77,23 @@ def plot_path(filename):
         x = track['x'][idx]
         y = track['y'][idx]
 
-        plt.plot(y,x,c=colours[rand[i]], linewidth=2.5)
+        plt.plot(y, x, c=colours[rand[i]], linewidth=2.5)
     plt.axis('equal')
     a = list(plt.axis())
-    a[3],a[2] = a[2], a[3]
+    a[3], a[2] = a[2], a[3]
     plt.axis(a)
 
     plt.gca().xaxis.set_major_locator(plt.NullLocator())
     plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    #plt.axis('off')
+    # plt.axis('off')
 
-    plt.savefig(saved_name+'/track.png')
-    plt.savefig(saved_name+'/track.pdf')
+    plt.savefig(saved_name + '/track.png')
+    plt.savefig(saved_name + '/track.pdf')
 
     plt.show()
 
+
 if __name__ == '__main__':
-    filename = tkFileDialog.askopenfilename(title='Locate a track.p file',filetypes=[("track.p file","*.p")])
+    filename = tkinter.filedialog.askopenfilename(
+        title='Locate a track.p file', filetypes=[("track.p file", "*.p")])
     plot_path(filename)
--- JBKirkegaard-multiwormtracker-cfe9c05d4830/settings.py
+++ JBKirkegaard-multiwormtracker-cfe9c05d4830/settings.py
@@ -1,4 +1,4 @@
-### Input
+# Input
 filename = "C:/Users/Mandy/Downloads/JBKirkegaard-multiwormtracker-85a540a2a163/D1_swimming_2016-04-01-122434-0000.avi"
 start_frame = 0
 limit_images_to = 10
@@ -7,20 +7,20 @@ px_to_mm = 1.0
 darkfield = False
 stop_after_example_output = False
 
-### Output
+# Output
 save_as = "C:/Users/Mandy/Downloads/JBKirkegaard-multiwormtracker-85a540a2a163/"
 output_overlayed_images = 0
-font_size =  8
-fig_size = (20,20)
+font_size = 8
+fig_size = (20, 20)
 scale_bar_size = 1.0
 scale_bar_thickness = 7
 
-### Z-filtering
+# Z-filtering
 use_images = 100
 use_around = 5
 Z_skip_images = 1
 
-### Thresholding
+# Thresholding
 keep_dead_method = False
 std_px = 64
 threshold = 8
@@ -30,26 +30,26 @@ skeletonize = False
 prune_size = 0
 do_full_prune = False
 
-### Locating
+# Locating
 min_size = 50
 max_size = 1000
 minimum_ecc = 0.7
 
-### Form trajectories
+# Form trajectories
 max_dist_move = 5
 min_track_length = 5
 memory = 5
 
-### Bending statistics
+# Bending statistics
 bend_threshold = 2.0
 minimum_bends = 0.0
 
-### Velocities
+# Velocities
 frames_to_estimate_velocity = 3
 
-### Dead worm statistics
+# Dead worm statistics
 maximum_bpm = 0.5
 maximum_velocity = 0.1
 
-### Regions
+# Regions
 regions = {}
